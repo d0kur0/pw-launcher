bae67c24e9fb09fef337511ecd2b10e7228bf5ab
d0kur0 <dev.dokuro@gmail.com>
2024-01-29 10:32:45 +0300

    fix: release assets

--- release.config.cjs
+++ release.config.cjs
@@ -6,12 +6,7 @@ module.exports = {
 		[
 			"@semantic-release/github",
 			{
-				assets: [
-					{ path: "gh_assets/webm-desktop_setup_amd64.exe", label: "Windows Installer" },
-					{ path: "gh_assets/webm-desktop-darwin-x64.zip", label: "MacOS app" },
-					{ path: "gh_assets/webm-desktop_amd64.deb", label: "Installer for Debian-based distro" },
-					{ path: "gh_assets/webm-desktop-1.x86_64.rpm", label: "Installer for RHat-based distro" },
-				],
+				assets: [{ path: "gh_assetspw-launcher_setup_amd64.exe", label: "Windows Installer" }],
 			},
 		],
 		[
